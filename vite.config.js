4be0a5bb4b6cb0b8867317e7199a1795d898401e
sarirkarim <sarirkarim755@gmail.com>
2023-07-20 20:42:19 +0500

    commit message

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/frontend-project/",
+  build: {
+    chunkSizeWarningLimit: 100000000
+},
   plugins: [react()],
 })
